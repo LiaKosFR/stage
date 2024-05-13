b518af52e995a79b648414f8019f4fe0f035cb1b
loico <loico@LiaKos>
2024-05-13 11:30:44 +0200

    caca pipi

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     </head>
     <body>
         <?php
-        caca
+        //caca pipi
         // put your code here
         ?>
     </body>
