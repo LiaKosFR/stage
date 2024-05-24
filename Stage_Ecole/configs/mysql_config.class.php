19e52572cf261d3e31294c6484fd22bfc7d26bba
loico <loico@LiaKos>
2024-05-24 16:33:52 +0200

    site ecole php avec probleme css v6

--- Stage_Ecole/configs/mysql_config.class.php
+++ Stage_Ecole/configs/mysql_config.class.php
@@ -1,10 +1,14 @@
 <?php
 
 class MysqlConfig {
-    const SERVEUR = 'sio1.btssio.net';
-    const BASE = 'tannou2';
-    const UTILISATEUR = 'tannou';
-    const MOT_DE_PASSE = 'twqmm';
+//    const SERVEUR = 'sio1.btssio.net';
+//    const BASE = 'tannou2';
+//    const UTILISATEUR = 'tannou';
+//    const MOT_DE_PASSE = 'twqmm';
+    const SERVEUR = 'localhost';
+    const BASE = 'stage';
+    const UTILISATEUR = 'root';
+    const MOT_DE_PASSE = '';
 }
 
 ?>
