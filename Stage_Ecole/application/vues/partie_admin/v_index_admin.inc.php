b96d9ed5f3002536bb1b217116622e418ae10688
tanno <tanno@Reeverrs.home>
2024-05-27 21:54:11 +0200

    admin continuer et base sur sio1.btssio.net

--- Stage_Ecole/application/vues/partie_admin/v_index_admin.inc.php
+++ Stage_Ecole/application/vues/partie_admin/v_index_admin.inc.php
@@ -6,24 +6,17 @@
     <div class ="produit">
         <h2 class="titre_modifier_ajouter_supp">Ajouter/Modifier/Supprimer un Produit</h2>
 
-        <form class="formAjouterProduit" method="POST" action="index.php?controleur=Produits&action=ajouterProduit">
+        <form class="formAjouterActualite" method="POST" action="index.php?controleur=Actualite&action=AjouterAcutalite">
             <input type="hidden" name="action" value="ajouterProduit">
-            <label for="nom">Nom du produit :</label>
-            <input type="text" id="nom" name="nom" placeholder= "Entrez nom du produit" required></br>
-            <label for="nom">Description du Produit :</label>
+            <label for="nom">Titre de l'actualite :</label>
+            <input type="text" id="Titre" name="Titre" placeholder= "Entrez le Titre de l'actualité" required></br>
+            <label for="nom">Description de l'actualite :</label>
             <input type="text" id="description" name="description" placeholder="Entrez la description" required></br>
-            <label for="nom">Prix du produit :</label>
-            <input type="text" id="prix" name="prix" placeholder="Entrez le prix du produit"required></br>
-            <label for="nom">Image du produit :</label>
+            <label for="nom">Image de l'actualite :</label>
             <input type="file" id="image" name="image"></br>
-            <label for="nom">Categorie du produit :</label>
-            <select id="categorie_produit" name="categorie_produit" required>
-                <option value="">Sélectionner une catégorie</option>
-                
-            </select></br>
-            <label for="nom">Fournisseur du produit :</label>
-            <select id="fournisseur" name="fournisseur" required placeholder= "Entrez nom du produit">
-                <option value="">Sélectionner un fournisseur</option>
+            <label for="nom">Visibilité de l'actualite :</label>
+            <select id="Privacy_actualite" name="Privacy_actualite" required>
+                <option value="">Sélectionner la visibilite de l'actualite</option>
                 
             </select></br>
             <input  class ="bouton_form" type="submit"  value="Ajouter"/>
@@ -31,34 +24,34 @@
 
 
 
-        <form  class="formModifierProduit" action="index.php?controleur=Categories&action=modifierCategorie" method="POST">
-            <label for="produit_a_modifier">Modifier Produit :</label>
+        <form  class="formModifierActualite" action="index.php?controleur=Categories&action=modifierCategorie" method="POST">
+            <label for="actualite_a_modifier">Modifier l'actualite :</label>
 
-            <select id="produit_a_modifier" name="produit_a_modifier" required>
-                <option value="">Sélectionner un produit</option>
+            <select id="actualite_a_modifier" name="actualite_a_modifier" required>
+                <option value="">Sélectionner l'actualite</option>
                 
 
             </select>
 
-            <input type="text" id="nouveau_nom_produit" name="nouveau_nom_produit" placeholder="Nouveau Nom" required></br>
-            <label for="nom">Description du produit :</label>
-            <input type="text" id="descrption_produit" name="descrption_produit" value="" required></br>
-            <label for="nom">Prix du produit :</label>
-            <input type="text" id="prix_produit" name="prix_produit" value="" required></br>
-            <label for="nom">Image du produit :</label>
-            <input type="text" id="image_produit" name="image_produit" value="" required></br>
-            <label for="nom">Categorie du produit :</label>
-            <input type="text" id="categorie_produit" name="categorie_produit" value="" required></br>
-            <label for="nom">Fournisseur du produit :</label>
-            <input type="text" id="fournisseur_produit" name="fournisseur_produit" value="" required></br>
+            <input type="text" id="nouveau_nom_actualite" name="nouveau_nom_actualite" placeholder="Nouveau Nom" required></br>
+            <label for="nom">Description de l'actualite :</label>
+            <input type="text" id="descrption_actualite" name="descrption_actualite" value="" required></br>
+            <label for="nom">Prix de l'actualite :</label>
+            <input type="text" id="prix_actualite" name="prix_actualite" value="" required></br>
+            <label for="nom">Image de l'actualite :</label>
+            <input type="text" id="image_actualite" name="image_actualite" value="" required></br>
+            <label for="nom">Categorie de l'actualite :</label>
+            <input type="text" id="categorie_actualite" name="categorie_actualite" value="" required></br>
+            <label for="nom">Fournisseur de l'actualite :</label>
+            <input type="text" id="fournisseur_actualite" name="fournisseur_actualite" value="" required></br>
 
             <input class ="bouton_form" type="submit" value="Modifier">
         </form>
 
         <form  class="formSuppProduit"action="index.php?controleur=Produits&action=supprimerProduit" method="POST">
-            <label for="produit_a_supprimer">Supprimer Catégorie :</label>
-            <select id="produit_a_supprimer" name="produit_a_supprimer" required>
-                <option value="">Sélectionner un produit</option>
+            <label for="actualite_a_supprimer">Supprimer une actualite</label>
+            <select id="actualite_a_supprimer" name="actualite_a_supprimer" required>
+                <option value="">Sélectionner l'actualite</option>
                
 
             </select>
