36600e7958e749ffa8eda28e1ae56a4a236dd3fb
tanno <tanno@Reeverrs.home>
2024-06-19 18:43:14 +0200

    menu qui change

--- Stage_Ecole/index.php
+++ Stage_Ecole/index.php
@@ -8,15 +8,19 @@ require_once Chemins::MODELES . 'gestion_boutique.class.php';
 require_once Chemins::CONFIGS . 'mysql_config.class.php';
 require_once Chemins::CONFIGS . 'variables_globales.class.php';
 require Chemins::VUES_PERMANENTES . 'v_entete.inc.php';
-if (isset($_SESSION['login'])) {
-    require Chemins::VUES_PERMANENTES . 'v_menu_logs.inc.php';
-
+if (isset($_SESSION['login_admin'])) {
+    // Incluez le menu admin
+    require Chemins::VUES_PERMANENTES . 'v_menu_admin.inc.php';
 } else {
-    require Chemins::VUES_PERMANENTES . 'v_menu.inc.php';
-}
-if (isset($_SESSION['logs'])) {
-        require Chemins::VUES_PERMANENTES . 'v_menu_admin.inc.php';
+    // Vérifiez si l'utilisateur est connecté
+    if (isset($_SESSION['login'])) {
+        // Incluez le menu pour les utilisateurs connectés
+        require Chemins::VUES_PERMANENTES . 'v_menu_logs.inc.php';
+    } else {
+        // Incluez le menu par défaut pour les utilisateurs non connectés
+        require Chemins::VUES_PERMANENTES . 'v_menu.inc.php';
     }
+}
 
 
 require_once Chemins::CONTROLEURS . 'ControleurActualite.class.php';
@@ -24,7 +28,7 @@ require_once Chemins::CONTROLEURS . 'ControleurActualite.class.php';
 if (!isset($_REQUEST['controleur'])) {
     require_once Chemins::VUES . 'v_accueil.inc.php';
 } else {
-
+    
     $classeControleur = 'Controleur' . $_REQUEST['controleur'];
     $fichierControleur = $classeControleur . ".class.php";
     require_once(Chemins::CONTROLEURS . $fichierControleur);
