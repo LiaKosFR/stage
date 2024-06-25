12fcf39f894288f332056736fc59f5b14f58153b
tanno <tanno@Reeverrs.home>
2024-06-25 17:23:18 +0200

    super admin commencé tout les menus sont fais

--- Stage_Ecole/configs/mysql_config.class.php
+++ Stage_Ecole/configs/mysql_config.class.php
@@ -1,7 +1,10 @@
 <?php
 
 class MysqlConfig {
-            
+    const SERVEUR = 'sio1.btssio.net';
+    const BASE = 'tannou2';
+    const UTILISATEUR = 'tannou';
+    const MOT_DE_PASSE = 'twqmm';
 //    const SERVEUR = 'localhost';
 //    const BASE = 'stage';
 //    const UTILISATEUR = 'root';
